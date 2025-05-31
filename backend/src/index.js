4d0c94ab7580b5264d3ef1b37891b3f1949f183e
Your Name <you@example.com>
2025-05-31 02:00:45 +0200

    Fix wildcard route for Render deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,9 +26,9 @@ app.use('/api/auth', authRoute);
 app.use('/api/message', messageRoute)
 
 if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.resolve(_dirname, "../frontend/dist")))
-  app.get("*", (req,res)=>{
-    res.sendFile(path.resolve(_dirname, "../frontend", "dist", "index.html"))
+  app.use(express.static(path.join(_dirname, "../frontend/dist")))
+  app.get("/*", (req,res)=>{
+    res.sendFile(path.join(_dirname, "../frontend", "dist", "index.html"))
   })
 }
 
